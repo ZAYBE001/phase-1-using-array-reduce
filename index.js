7b76dc655725397dafe8aac5ae8680252dee38f5
zaybe <zaybe.maliq@student.moringaschool.com>
2025-03-23 11:59:39 +0300

    add new files

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+function sumBatteries(total, batch) {
+    return total + batch;
+}
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce(sumBatteries, 0);
+
+console.log(totalBatteries);
